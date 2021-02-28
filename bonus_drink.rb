4b9b00bb3f75f10ad0889fa5e71d31a67c628cbb
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-02-28 12:20:16 +0900

    Solve bonus-drink

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,10 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    if amount / 3 == 0 then
+      return amount % 3
+    else
+      return amount - amount % 3 + total_count_for(amount / 3 + amount % 3)
+    end
   end
 end
+
